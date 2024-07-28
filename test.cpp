83e400ea4f5c887fec448fa0839366175eb75d48
Suvidha <suvidhadeshwal3@gmail.com>
2024-07-28 14:29:42 +0530

    first commit

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c,d,e;
+    cout<<"Enter the nos. : ";
+    cin>>a>>b;
+    cout<<a+b<<endl;
+    cin>>d>>e;
+    cout<<d*e;
+    
+}
